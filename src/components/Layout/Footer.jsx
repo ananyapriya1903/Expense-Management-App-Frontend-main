4ff7f4ed4d5070b8524294fb1bf5be43375834d0
Ananya Priya <ananya.priya@intermesh.com>
2024-11-09 19:38:04 +0530

    first commit

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-4">
+      <h6 className="text-center text-white">
+        2024 Semester Project - Expense Management System
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
